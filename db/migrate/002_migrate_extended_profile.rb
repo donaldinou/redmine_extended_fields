183894d6f97a8e41cb8bbb8bc059b53b49093d3a
s-andy <s-andy@c3fe15c0-a665-4a5f-9784-eb59f7be3c52>
2011-10-30 18:45:19 +0000

    Finished migration script

--- db/migrate/002_migrate_extended_profile.rb
+++ db/migrate/002_migrate_extended_profile.rb
@@ -1,21 +1,19 @@
-# TODO: add :is_for_new and :hint
-
 class MigrateExtendedProfile < ActiveRecord::Migration # PROFILE
 
     def self.up
         pos = UserCustomField.maximum(:position) || 0
+        settings = Setting.respond_to?(:plugin_extended_profile_plugin) ? Setting.plugin_extended_profile_plugin : nil
 
         company      = UserCustomField.create(:name => 'Company',             :field_format => 'string',  :position => pos += 1, :is_for_new => false, :searchable => true)
         company_site = UserCustomField.create(:name => 'Company website',     :field_format => 'link',    :position => pos += 1, :is_for_new => false)
         position     = UserCustomField.create(:name => 'Position',            :field_format => 'string',  :position => pos += 1, :is_for_new => false, :searchable => true)
-        project      = UserCustomField.create(:name => 'Project of interest', :field_format => 'project', :position => pos += 1, :visible    => false, :searchable => true) # FIXME: required from settings
+        project      = UserCustomField.create(:name => 'Project of interest', :field_format => 'project', :position => pos += 1, :visible    => false, :searchable => true, :is_required => (settings && settings[:require_project]) ? true : false)
         website      = UserCustomField.create(:name => 'Personal website',    :field_format => 'link',    :position => pos += 1, :is_for_new => false)
         blog         = UserCustomField.create(:name => 'Blog',                :field_format => 'link',    :position => pos += 1, :is_for_new => false)
         facebook     = UserCustomField.create(:name => 'Facebook',            :field_format => 'string',  :position => pos += 1, :is_for_new => false, :regexp => '^([0-9]+|[A-Za-z0-9.]+)$', :hint => 'e.g. 100000066953233 or andriy.lesyuk')
         twitter      = UserCustomField.create(:name => 'Twitter',             :field_format => 'string',  :position => pos += 1, :is_for_new => false, :regexp => '^@?[A-Za-z0-9_]+$',        :hint => 'e.g. AndriyLesyuk')
         linkedin     = UserCustomField.create(:name => 'LinkedIn',            :field_format => 'link',    :position => pos += 1, :is_for_new => false)
 
-        # TODO: migrate data
         User.find(:all).each do |user|
             CustomValue.create(:custom_field => company,      :customized => user, :value => user.profile[:company])       if user.profile[:company].present?
             CustomValue.create(:custom_field => company_site, :customized => user, :value => user.profile[:company_site])  if user.profile[:company_site].present?
