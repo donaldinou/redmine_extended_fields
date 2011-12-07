b78e6881c93e4e9b20219dcc2eae22a1cff5bf28
s-andy <s-andy@c3fe15c0-a665-4a5f-9784-eb59f7be3c52>
2011-12-07 20:29:07 +0000

    Fixed patch code

--- lib/extended_custom_field_patch.rb
+++ lib/extended_custom_field_patch.rb
@@ -3,48 +3,48 @@ require_dependency 'custom_field'
 module ExtendedCustomFieldPatch
 
     def self.included(base)
-        base.send(:include, InstanceMethods)
-        base.class_eval do
-            unloadable
+        if base.method_defined?(:possible_values_options)
+            base.send(:include, AliasPossibleValuesOptionsMethod)
+            base.class_eval do
+                unloadable
 
-            if defined? possible_values_options
                 alias_method_chain :possible_values_options, :extended
             end
+        else
+            base.send(:include, OverridePossibleValuesMethod)
         end
     end
 
-    module InstanceMethods
+    module AliasPossibleValuesOptionsMethod
 
-        if defined? possible_values_options
-
-            def possible_values_options_with_extended(obj = nil)
-                case field_format
-                when 'project'
-                    if obj.is_a?(User)
-                        projects = Project.visible(obj).all
-                    else
-                        projects = Project.visible.all
-                    end
-                    projects.collect{ |project| [ project.name, project.id.to_s ] }
+        def possible_values_options_with_extended(obj = nil)
+            case field_format
+            when 'project'
+            if obj.is_a?(User)
+                    projects = Project.visible(obj).all
                 else
-                    possible_values_options_without_extended(obj)
+                    projects = Project.visible.all
                 end
+                projects.collect{ |project| [ project.name, project.id.to_s ] }
+            else
+                possible_values_options_without_extended(obj)
             end
-
-        else
-
-            def possible_values(dummy = nil)
-                case field_format
-                when 'project'
-                    Project.visible.all.collect{ |project| [ project.name, project.id.to_s ] }
-                else
-                    read_attribute(:possible_values)
-                end
-            end
-
-            alias_method :possible_values_options, :possible_values
-
         end
+
+    end
+
+    module OverridePossibleValuesMethod
+
+        def possible_values(dummy = nil)
+            case field_format
+            when 'project'
+                Project.visible.all.collect{ |project| [ project.name, project.id.to_s ] }
+            else
+                read_attribute(:possible_values)
+            end
+        end
+
+        alias_method :possible_values_options, :possible_values
 
     end
 
