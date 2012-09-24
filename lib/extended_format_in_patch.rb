4e06873e16458f4b15bb405615dcf1571f0ff2f6
s-andy <s-andy@c3fe15c0-a665-4a5f-9784-eb59f7be3c52>
2012-09-24 21:17:50 +0000

    Fixed dummy mistakes...

--- lib/extended_format_in_patch.rb
+++ lib/extended_format_in_patch.rb
@@ -15,14 +15,14 @@ module ExtendedFormatInPatch
 
         def format_in_with_extended?(*args)
             case field_format
-            when 'wiki':
+            when 'wiki'
                 args.include?('text')
-            when 'link':
+            when 'link'
                 args.include?('string')
-            when 'project':
+            when 'project'
                 args.include?('version')
             else
-                format_in_without_extended?(*arg)
+                format_in_without_extended?(*args)
             end
         end
 
