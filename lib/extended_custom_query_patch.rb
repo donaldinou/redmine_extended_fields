26d1047e22d17e934f61dacfa02f3a0154075a0e
s-andy <s-andy@c3fe15c0-a665-4a5f-9784-eb59f7be3c52>
2013-01-19 23:12:10 +0000

    Fixed wrong number of arguments

--- lib/extended_custom_query_patch.rb
+++ lib/extended_custom_query_patch.rb
@@ -3,7 +3,12 @@ require_dependency 'query'
 module ExtendedCustomQueryPatch
 
     def self.included(base)
-        base.send(:include, InstanceMethods)
+        if Redmine::VERSION::MAJOR < 2 # FIXME: Maybe MINOR < 1?
+            base.send(:include, Redmine1InstanceMethods)
+        else
+            base.send(:include, Redmine2InstanceMethods)
+        end
+
         base.class_eval do
             unloadable
 
@@ -11,7 +16,29 @@ module ExtendedCustomQueryPatch
         end
     end
 
-    module InstanceMethods
+    module Redmine2InstanceMethods
+
+        def add_custom_fields_filters_with_extended(custom_fields, assoc = nil)
+            add_custom_fields_filters_without_extended(custom_fields, assoc)
+
+            custom_fields.select(&:is_filter?).each do |field|
+                case field.field_format
+                when "project"
+                    options = { :type => :list_optional, :values => field.possible_values_options, :order => 20 }
+                end
+                filter_id = "cf_#{field.id}"
+                filter_name = field.name
+                if assoc.present?
+                    filter_id = "#{assoc}.#{filter_id}"
+                    filter_name = l("label_attribute_of_#{assoc}", :name => filter_name)
+                end
+                @available_filters[filter_id] = options.merge({ :name => filter_name, :format => field.field_format }) if options
+            end
+        end
+
+    end
+
+    module Redmine1InstanceMethods
 
         def add_custom_fields_filters_with_extended(custom_fields)
             add_custom_fields_filters_without_extended(custom_fields)
